f7effd39c43c102a52d0192881c422415f6b261b
WilliamMass <27826084+WilliamMass@users.noreply.github.com>
2020-01-23 16:43:50 -0800

    Changes to CLI

--- WB-DataGenerator.py
+++ WB-DataGenerator.py
@@ -10,18 +10,24 @@ import csv
 
 in_menu = True
 
+def print_options():
+    print("")
+    print("The following options are available:")
+    print("1) Generate all data.")
+    print("0) Exit\n")
+
+
 def CLI(in_menu):
     while in_menu == True:
         print("Wisconsin Benchmark Data Generator will produce data for Wisconsin Benchmark categories.\n")
-        print("The following options are available:")
-        print("1) Generate all data.")
-        print("0) Exit\n")
+        print_options()
         while True:
             try:
                 selection = int(input("Please make a selection:"))
                 break
             except:
                 print("That's not a valid option! Please stop typing in nonsense.")
+                print_options()
                 continue
         if selection == 0:
             in_menu = False
@@ -30,6 +36,7 @@ def CLI(in_menu):
         else:
             print("That's not a valid option! Please select an option from the menu.")
     print("Exiting program! Thanks for using Wisconsin Benchmark Data Generator!")
+
 
 def benchmarking_scriptwriter():
         print("Obviously, we make this do something")
